84918dc1ec292c770348ec2cf8b321462903e74d
jahedsabbir <jahed.sabbir.bd@gmail.com>
2020-10-10 10:22:39 +0600

    first commit

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Services from '../Services/Services';
+import FeaturedService from '../FeaturedService/FeaturedService';
+import MakeAppointment from '../MakeAppoinment/MakeAppointment';
+import Blogs from '../Blogs/Blogs';
+import Doctors from '../Doctors/Doctors';
+import Contact from '../Contact/Contact';
+import Footer from '../Footer/Footer';
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Services></Services>
+            <FeaturedService></FeaturedService>
+            <MakeAppointment></MakeAppointment>
+             <Blogs></Blogs>
+             <Doctors></Doctors>
+             <Contact></Contact> 
+             <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
